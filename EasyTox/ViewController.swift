67203c8935db60e875020b05e16b83137d1c15f3
Satish <iksatish.k@gmail.com>
2016-02-20 06:36:48 -0800

    Initial Commit

---
+++ EasyTox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyTox
+//
+//  Created by Satish Kumar R Kancherla on 2/20/16.
+//  Copyright © 2016 Satish Kumar R Kancherla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
